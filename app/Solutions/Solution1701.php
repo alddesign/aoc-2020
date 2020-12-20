2616c040e74f46df1a16ff4da6c09b38c7f8ab4b
alddesign <alexander.doepper@aon.at>
2020-12-20 02:05:04 +0100

    Day 16 & 17

--- app/Solutions/Solution1701.php
+++ app/Solutions/Solution1701.php
@@ -2,14 +2,250 @@
 declare(strict_types = 1);
 namespace Alddesign\EzMvc\Solutions;
 
+use Alddesign\EzMvc\System\Helper;
+
 class Solution1701 extends Solution
 {
-	public $ready = false;
+	public $ready = true;
+
+	private const INPUT =
+	[
+		'.##..#.#',
+		'#...##.#',
+		'##...#.#',
+		'.##.##..',
+		'...#.#.#',
+		'.##.#..#',
+		'...#..##',
+		'###..##.'
+	];
+
+	//Test
+	private const INPUT1 = 
+	[
+		'.#.',
+		'..#',
+		'###'
+	];
+
+	/** @var int[][][]  */
+	private $pocketDimension = [[[]]]; //[x][y][z] => true|false
+	private $dimensionSteps = [];
 	
 	public function run()
 	{
 		set_time_limit(0);
-		
 
+		$this->initDimension();
+		$this->dimensionSteps[] = $this->pocketDimension;
+		for($i = 1; $i <= 6; $i++)
+		{
+			$this->expandDimension();
+			$this->updateDimension();			
+			$this->dimensionSteps[] = $this->pocketDimension;
+		}
+
+
+		$active = $this->countCubes(1);
+
+		$this->outputJsonData();
+	}
+
+	private function outputJsonData()
+	{
+		//rebuilding array indexes... JS is not so cool with indexes like -1, -2,...
+		foreach($this->dimensionSteps as $step => $dimensionStep)
+		{
+			$this->dimensionSteps[$step] = array_values($this->dimensionSteps[$step]);
+			foreach($this->dimensionSteps[$step] as $z => $zDim)
+			{
+				$this->dimensionSteps[$step][$z] = array_values($this->dimensionSteps[$step][$z]);
+				foreach($this->dimensionSteps[$step][$z] as $y => $yDim)
+				{
+					$this->dimensionSteps[$step][$z][$y] = array_values($this->dimensionSteps[$step][$z][$y]);
+				}
+			}			
+		}
+
+		echo "<script>var steps = ";
+		echo  json_encode($this->dimensionSteps);
+		echo ";</script>\n";
+		echo '<script src="'.Helper::url('assets/three.min.js').'"></script>'. "\n";
+		echo '<script src="'.Helper::url('assets/Solution17.js').'"></script>'. "\n";
+	}
+
+
+	/**
+	 * PHP has an internal array index that is different from ours...
+	 */
+	private function sortDimension()
+	{
+		ksort($this->pocketDimension);
+		foreach($this->pocketDimension as $z => $zDim)
+		{
+			ksort($this->pocketDimension[$z]);
+			foreach($this->pocketDimension[$z] as $y => $yDim)
+			{
+				ksort($this->pocketDimension[$z][$y]);
+			}
+		}
+	}
+
+	/**
+	 * Prints the dimension like in the example
+	 * @param bool $die Quit after
+	 */
+	private function printDim(bool $die = true)
+	{
+		$this->sortDimension();
+
+		foreach($this->pocketDimension as $z => $zDim)
+		{
+			echo "<br><b>Z$z</b><br>";
+			foreach($this->pocketDimension[$z] as $y => $yDim)
+			{
+				$yno = str_pad(strval($y), 3, '_', STR_PAD_LEFT);
+				echo "$yno:";
+				foreach($this->pocketDimension[$z][$y] as $x => $value)
+				{
+					echo $value ? '#' : '.';
+				}
+				echo "<br>";
+			}
+		}
+		if($die)
+		{
+			die;
+		}
+	}
+
+	private function countCubes(int $which = 1) //0 = inactive, 1 = active, 2 = all;
+	{
+		$active = 0;
+		$all = 0;
+		foreach($this->pocketDimension as $z => $zDim)
+		{
+			foreach($this->pocketDimension[$z] as $y => $yDim)
+			{
+				foreach($this->pocketDimension[$z][$y] as $x => $value)
+				{
+					$active += $value ? 1 : 0;
+					$all++;
+				}
+			}
+		}
+
+		$inactive = $all - $active;
+
+		switch($which)
+		{
+			case(0) : return $inactive;
+			case(1) : return $active;
+			default : return $all;
+		}
+	}
+
+	private function updateDimension()
+	{	
+		$this->sortDimension();
+
+		$newPocketDimension = $this->pocketDimension;
+		foreach($this->pocketDimension as $z => $zDim)
+		{
+			foreach($this->pocketDimension[$z] as $y => $yDim)
+			{
+				foreach($this->pocketDimension[$z][$y] as $x => $value)
+				{
+					$active = $this->countNearbyActive($z,$y,$x);
+					if($value)
+					{
+						$newPocketDimension[$z][$y][$x] = ($active === 2 || $active === 3) ? true : false;
+					}
+					else
+					{
+						$newPocketDimension[$z][$y][$x] = $active === 3 ? true : false;
+					}
+				}
+			}
+		}
+
+		$this->pocketDimension = $newPocketDimension;
+		unset($newPocketDimension);
+	}
+
+	private function countNearbyActive(int $z, int $y, int $x)
+	{
+		$active = 0;
+		for($z1 = $z-1; $z1 <= $z + 1; $z1++)
+		{
+			for($y1 = $y-1; $y1 <= $y + 1; $y1++)
+			{
+				for($x1 = $x-1; $x1 <= $x + 1; $x1++)
+				{
+					if(isset($this->pocketDimension[$z1][$y1][$x1]) && $this->pocketDimension[$z1][$y1][$x1] === true && ($z !== $z1 || $y !== $y1 || $x !== $x1))
+					{
+						$active++;
+					}
+				}
+			}
+		}
+
+		return $active;
+	}
+
+	
+	/**
+	 * After the dimension is initialized or calculated, we need to expand by 1 in each x,y,z with FALSE
+	 * 
+	 * @return void
+	 */
+	private function expandDimension()
+	{
+		$this->sortDimension();
+
+		$newPocketDimension = $this->pocketDimension;
+		foreach($this->pocketDimension as $z => $zDim)
+		{
+			foreach($this->pocketDimension[$z] as $y => $yDim)
+			{
+				foreach($this->pocketDimension[$z][$y] as $x => $value)
+				{
+					$this->setNearbyInactive($z,$y,$x,$newPocketDimension);
+				}
+			}
+		}
+
+		$this->pocketDimension = $newPocketDimension;
+		unset($newPocketDimension);
+	}
+
+	private function setNearbyInactive(int $z, int $y, int $x, array &$newPocketDimension)
+	{
+		for($z1 = $z-1; $z1 <= $z + 1; $z1++)
+		{
+			for($y1 = $y-1; $y1 <= $y + 1; $y1++)
+			{
+				for($x1 = $x-1; $x1 <= $x + 1; $x1++)
+				{
+					if(!isset($newPocketDimension[$z1][$y1][$x1]))
+					{
+						$newPocketDimension[$z1][$y1][$x1] = false;
+					}
+				}
+			}
+		}
+	}
+
+	private function initDimension()
+	{
+		$l = strlen($this::INPUT[0]);
+		$z = 0; //i guess thats the case here!?
+		foreach($this::INPUT as $y => $yDim)
+		{
+			for($x = 0; $x < $l; $x++)
+			{
+				$this->pocketDimension[$z][$y][$x] = $yDim[$x] === '#' ? true : false;
+			}
+		}
 	}
 }
---
+++ assets/Solution17.js
@@ -0,0 +1,177 @@
+var camera;
+var cameraLookAt;
+var cameraPos;
+var cameraOffsetZ = 7;
+var render;
+var scene;
+var light;
+var lx = 0;
+var ly = 0;
+var lz = 0;
+var testCube;
+var animationId = null;
+var stepNo = 6;
+var rotationSpeed = 0.0004;
+var fps = 60;
+var w = 0;
+var h = 0;
+
+$(document).ready(function()
+{
+	window.requestAnimationFrame = (function() {
+		return window.requestAnimationFrame;
+	})();
+	
+	var content = $('#content');
+	var canvas;
+	w = window.innerWidth - 32;
+	h = window.innerHeight - 200;
+
+	function initCanvas()
+	{
+		content.append($(`<div id="canvas" width="${w}" height="${h}"></div>`));
+		canvas = document.getElementById('canvas');
+
+		render = new THREE.WebGLRenderer();
+		render.setClearColor(0xffffff, 1);
+		render.setSize(w, h);
+
+		canvas.appendChild(render.domElement);
+		$('canvas').css('border', '1px solid #ccc');
+	}
+
+	function startScene(step)
+	{
+		scene = new THREE.Scene();
+		var aspect = w / h;
+
+		camera = new THREE.PerspectiveCamera(45, aspect);
+		scene.add(camera);
+		
+		light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+		scene.add(light);
+
+		createCubesFromDim(step);
+
+		renderScene();
+	}
+
+	function createCubesFromDim(step)
+	{
+		var dim = steps[step];
+		lz = dim.length;
+		for(var z = 0; z < lz; z++)
+		{
+			ly = dim[z].length;
+			for(var y = 0; y < ly; y++)
+			{
+				lx = dim[z][y].length;
+				for(var x = 0; x < lx; x++)
+				{
+					if(dim[z][y][x])
+					{
+						scene.add(createCube(x,-y,z));
+					}
+				}	
+			}	
+		}
+
+		cameraPos = new THREE.Vector3((lx-1)/2,0, lx + cameraOffsetZ);
+		camera.position.copy(cameraPos);
+
+		cameraLookAt = {x: (lx-1)/2, y : (-ly+1)/2, z: (lz-1) / 2};
+		camera.lookAt(cameraLookAt);
+
+		testCube = createCube((lx-1)/2, (-ly+1)/2, (lz-1) / 2, 0xcc3333);
+		scene.add(testCube);
+
+	}
+
+	function createCube(x,y,z,color) 
+	{
+
+		var color = color || 0x33cc33;
+		
+		var cubeMaterials = [
+
+			new THREE.MeshLambertMaterial({color:color}),
+            new THREE.MeshLambertMaterial({color:color}),
+            new THREE.MeshLambertMaterial({color:color}),
+            new THREE.MeshLambertMaterial({color:color}),
+            new THREE.MeshLambertMaterial({color:color}),
+			new THREE.MeshLambertMaterial({color:color})
+        ];
+
+		var cubeSize = 0.8;
+		var cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
+		var cubeGeometry = new THREE.BoxGeometry(cubeSize,cubeSize,cubeSize);
+
+		cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+		cube.position.set(x,y,z);
+
+		return cube;
+	}
+
+	function animateScene() 
+	{
+		setTimeout( function() {animationId = requestAnimationFrame(animateScene);}, 1000 / fps );
+
+		
+		var speed = Date.now() * rotationSpeed;
+		camera.position.x = Math.cos(speed) * cameraPos.z + cameraLookAt.x;
+		camera.position.z = Math.sin(speed) * cameraPos.z + cameraLookAt.z;
+
+		camera.lookAt(cameraLookAt);
+		
+		renderScene();
+	}
+
+	function stop()
+	{
+		if(animationId)
+		{
+			cancelAnimationFrame(animationId);
+			animationId = null;
+
+			$('#canvas').remove();
+		}
+	}
+
+	function renderScene()
+	{
+		render.render(scene, camera);
+	}
+
+	function start(s)
+	{
+		stepNo += s;
+		stepNo = stepNo < 0 ? 0 : stepNo;
+		stepNo = stepNo > 6 ? 6 : stepNo;
+
+		stop();
+
+		$('#steptext').text(`Step ${stepNo}`);
+
+		initCanvas();
+		startScene(stepNo);
+		animateScene();
+	}
+
+	function initGui()
+	{
+		var prevButton = $(`<button id="prev" class="btn btn-primary">&lt;&lt; Prev.</button>`);
+		var stepText = $(`<span id="steptext" class="h4" style="margin: 0 10px; vertical-align: middle;">Step 0</span>`);
+		var nextButton = $(`<button id="next" class="btn btn-primary">Next &gt;&gt;</button>`);
+
+		prevButton.click(function(){ start(-1); });
+		nextButton.click(function(){ start(+1); });
+
+		$('#container').prepend($(nextButton));
+		$('#container').prepend($(stepText));
+		$('#container').prepend($(prevButton));
+	}
+
+	initGui();
+	start(stepNo);	
+
+});
